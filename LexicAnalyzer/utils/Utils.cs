b9a4ecbe5e3ecd44c6a7ff0def1bd0c5172750fb
gcasq <gustavoacasqueiro@ime.eb.br>
2022-09-03 14:57:24 -0300

    function search keyword and return corresponding token

---
+++ LexicAnalyzer/utils/Utils.cs
@@ -0,0 +1,17 @@
+﻿using static LexicAnalyzer.Enums;
+
+namespace LexicAnalyzer.utils
+{
+    public class Utils {
+        public static t_token SearchKeyWord(string name) {
+            t_token token;
+            try {
+                token = (t_token)System.Enum.Parse(typeof(t_token), name, true);
+            }
+            catch (ArgumentException e) {
+                return t_token.UNKNOWN;
+            }
+            return token;
+        }
+    }
+}
